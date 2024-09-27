a290ffc43c185ba9126c70b3c063ef92aa2a07ae
abdul-dulal <dulal.dpi.387364@gmail.com>
2024-09-26 21:18:16 -0600

    video modal update

--- components/home3/Slider.js
+++ components/home3/Slider.js
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { PiArrowRightDuotone } from "react-icons/pi";
 import { rubik } from "@/app/fonts";
 import VideoModal from "../ui/VideoModal";
+import SliderVideoModal from "./SliderVideoModal";
 
 const Slider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -132,11 +133,13 @@ const Slider = () => {
                   <br className="xs:block hidden" /> Technology Solution
                 </h2>
                 <div
-                  className={`layer_animated mt-8 ${
-                    activeIndex == 1 ? "video" : ""
+                  className={`  mt-8 ${
+                    activeIndex == 1
+                      ? "opacity-100 visible transition-all delay-[1.9s]   duration-500 ease-in-out"
+                      : "opacity-0 invisible"
                   }`}
                 >
-                  <VideoModal />
+                  <SliderVideoModal />
                 </div>
               </div>
             </div>
@@ -213,14 +216,14 @@ const Slider = () => {
 
       <button
         onClick={() => swiperRef.current?.slidePrev()}
-        className="slider-button absolute left-0 top-[60%] transform -translate-y-1/2 p-4 bg-primary text-white rounded-full cursor-pointer opacity-0 group-hover:opacity-100 transition-all ease-out duration-300    group-hover:left-5 group-hover:transition-all group-hover:ease-out group-hover:duration-300 z-10  after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0 after:m-auto after:content-[''] after:rounded-[50%] after:text-center after:w-0 after:h-0 after:-z-[1] after:transition-all after:ease-out-out after:duration-300 after:bg-[#081845]"
+        className="slider-button absolute left-0 top-[50%] transform -translate-y-1/2 p-4 bg-white text-primary rounded-full cursor-pointer opacity-0 group-hover:opacity-100 transition-all ease-out duration-300    group-hover:left-14 group-hover:transition-all group-hover:ease-out group-hover:duration-300 z-10  after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0 after:m-auto after:content-[''] after:rounded-[50%] after:text-center after:w-0 after:h-0 after:-z-[1] after:transition-all after:ease-out-out after:duration-300 after:bg-[#081845]"
       >
         <LuArrowLeft size={22} />
       </button>
 
       <button
         onClick={() => swiperRef.current?.slideNext()}
-        className="slider-button absolute right-0 top-[60%] transform -translate-y-1/2 p-4 bg-primary text-white rounded-full cursor-pointer opacity-0 group-hover:opacity-100 transition-all ease-out duration-300 z-10 hover:bg-primary  group-hover:right-5 group-hover:transition-all group-hover:ease-out group-hover:duration-300 after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0 after:m-auto after:content-[''] after:rounded-[50%] after:text-center after:w-0 after:h-0 after:-z-[1] after:transition-all after:ease-out-out after:duration-300 after:bg-[#081845]"
+        className="slider-button absolute right-0 top-[50%] border-none transform -translate-y-1/2 p-4 bg-white text-primary rounded-full cursor-pointer opacity-0 group-hover:opacity-100 transition-all ease-out duration-300 z-10 hover:bg-primary  group-hover:right-14 group-hover:transition-all group-hover:ease-out group-hover:duration-300 after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0 after:m-auto after:content-[''] after:rounded-[50%] after:text-center after:w-0 after:h-0 after:-z-[1] after:transition-all after:ease-out-out after:duration-300 after:bg-[#081845]"
       >
         <HiArrowRight size={22} />
       </button>
---
+++ components/home3/SliderVideoModal.js
@@ -0,0 +1,56 @@
+"use client";
+import React, { useState } from "react";
+
+import { FaPlay } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+const SliderVideoModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closePopup = (e) => {
+    e.stopPropagation();
+  };
+  return (
+    <div>
+      <button
+        style={{ animationDelay: "2.4s" }}
+        className="popup-video  w-20 h-20  leading-[85px]  relative -top-[22px] rounded-[50%] text-center inline-block text-xl bg-white text-primary z-[99] ml-11 before:absolute before:content-[''] before:border-2 before:border-solid before:border-white before:z-0 before:left-[50%] before:top-[50%] before:-translate-x-[50%] before:block before:w-[160px] before:h-[160px] before:rounded-[50%] after:absolute after:content-[''] after:border-2 after:border-solid after:border-white after:z-0 after:left-[50%] after:top-[50%] after:-translate-x-[50%] after:-translate-y-[50%] after:block after:w-[160px] after:h-[160px] after:rounded-[50%] "
+        onClick={() => setIsOpen(true)}
+      >
+        <FaPlay className="m-auto" />
+      </button>
+
+      {isOpen && (
+        <div
+          onClick={togglePopup}
+          className="fixed inset-0 z-[1001] flex items-center justify-center bg-black bg-opacity-75"
+        >
+          <button
+            onClick={togglePopup}
+            className="h-8 w-8 flex items-center justify-center p-[2px] rounded-full border border-solid border-gray-300 absolute top-2 right-2 text-2xl text-white"
+          >
+            <RxCross2 />
+          </button>
+          <div
+            onClick={closePopup}
+            className="relative w-[65%] h-[65%] bg-white rounded"
+          >
+            <iframe
+              className="w-full h-full"
+              src="https://www.youtube.com/embed/SZEflIVnhH8?si=BH39mJbQuwQ-VpmA"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SliderVideoModal;
