d9085ead142c8ebc88b5e4c515ee55c73ca22c7d
abdul-dulal <dulal.dpi.387364@gmail.com>
2024-10-15 10:10:37 -0600

    home shuffle issue solved

--- components/home1/homeShuffle.js
+++ components/home1/homeShuffle.js
@@ -5,12 +5,13 @@ import React, { useEffect, useRef, useState } from "react";
 import Shuffle from "shufflejs";
 import { FaArrowRight } from "react-icons/fa6";
 import CategoryList from "../ui/CategoryList";
-import data from "/public/Portfolio.json";
+import portfolios from "/public/Portfolio.json";
 
 const HomeShuffle = () => {
   const shuffleContainer = useRef(null);
   const shuffleInstance = useRef(null);
   const [techCategories, setTechCategories] = useState([]);
+  const portfolio = portfolios.slice(1, 7);
 
   useEffect(() => {
     shuffleInstance.current = new Shuffle(shuffleContainer.current, {
@@ -24,7 +25,7 @@ const HomeShuffle = () => {
   }, []);
 
   useEffect(() => {
-    const categories = [...new Set(data.map((item) => item.tech))];
+    const categories = [...new Set(portfolios.map((item) => item.tech))];
     setTechCategories(categories);
   }, []);
 
@@ -40,53 +41,55 @@ const HomeShuffle = () => {
       />
       <div className="">
         <div ref={shuffleContainer} className="row my-shuffle-container">
-          {data.slice(0, 6).map((item, index) => {
-            const encTitle = encodeURIComponent(item.title);
-            const encTech = encodeURIComponent(item.tech);
-            <figure
-              key={item.id}
-              className={`shuffle-img picture-item image-${index + 1}`}
-              data-groups={JSON.stringify([item.tech])}
-            >
-              <div className="relative h-full w-full overflow-hidden">
-                <div className="folio-item-1 group mb-[30px] relative overflow-hidden after:absolute after:content-[''] after:left-0 after:top-0 after:w-full after:h-0 after:z-[1] after:opacity-0 after:invisible after:transition-all after:ease-in-out after:duration-300 after:bg-[rgba(104,110,226,0.8)] hover:after:h-full hover:after:opacity-[1] hover:after:visible">
-                  <Image
-                    src={item.img}
-                    alt={item.title}
-                    width={365}
-                    height={400}
-                    className="w-full"
-                  />
-                  <div
-                    style={{ width: "calc(100% - 30px)" }}
-                    className="folio-content absolute left-[15px] bottom-0 z-[2] bg-white rounded-[5px] pt-[23px] xs:pr-[70px] pr-5 pb-[16px] pl-[20px] invisible opacity-0 transition-all duration-300 ease-in-out group-hover:visible group-hover:bottom-4 group-hover:opacity-[1] group-hover:delay-[0.4s]"
-                  >
-                    <p className="relative text-primary mb-[2px]">
-                      <Link
-                        href={`/portfolioCategory/${encTech}`}
-                        className="text-primary inline-block hover:text-[#191919]"
-                      >
-                        {item.tech}
-                      </Link>
-                    </p>
-                    <h5 className="xs:inline-block block text-lg mb-0 hover:text-primary">
-                      <Link
-                        href={`portfolio/${encTitle}`}
-                        className="inline-block"
-                      >
-                        {item.title}
-                      </Link>
-                    </h5>
-                    <Link
-                      className="flex items-center justify-center w-11 h-11 bg-[rgba(104,110,226,0.1)] text-primary text-[16px] rounded-[3px] leading-[47px] text-center xs:absolute top-0 bottom-0 right-4 hover:bg-primary hover:text-white xs:m-auto mt-3"
-                      href={`portfolio/${encTitle}`}
+          {portfolio.map(({ id, img, tech, title }) => {
+            const encTitle = encodeURIComponent(title);
+            const encTech = encodeURIComponent(tech);
+            return (
+              <figure
+                key={id}
+                className="shuffle-img  picture-item"
+                data-groups={JSON.stringify([tech])}
+              >
+                <div className="relative h-full w-full overflow-hidden">
+                  <div className="folio-item-1 group mb-[30px] relative overflow-hidden after:absolute after:content-[''] after:left-0 after:top-0 after:w-full after:h-0 after:z-[1] after:opacity-0 after:invisible after:transition-all after:ease-in-out after:duration-300 after:bg-[rgba(104,110,226,0.8)] hover:after:h-full hover:after:opacity-[1] hover:after:visible">
+                    <Image
+                      src={img}
+                      alt={title}
+                      width={365}
+                      height={400}
+                      className=" w-full"
+                    />
+                    <div
+                      style={{ width: "calc(100% - 30px)" }}
+                      className="folio-content absolute left-[15px] bottom-0 z-[2] bg-white rounded-[5px] pt-[23px] xs:pr-[70px] pr-5 pb-[16px] pl-[20px] invisible opacity-0 transition-all duration-300 ease-in-out group-hover:visible group-hover:bottom-4 group-hover:opacity-[1] group-hover:delay-[0.4s]"
                     >
-                      <FaArrowRight />
-                    </Link>
+                      <p className=" relative text-primary mb-[2px]">
+                        <Link
+                          href={`/portfolioCategory/${encTech}`}
+                          className="text-primary inline-block hover:text-[#191919]"
+                        >
+                          {tech}
+                        </Link>
+                      </p>
+                      <h5 className="text-lg mb-0 hover:text-primary">
+                        <Link
+                          href={`portfolio/${encTitle}`}
+                          className="inline-block"
+                        >
+                          {title}
+                        </Link>
+                      </h5>
+                      <Link
+                        href={`/portfolio/${encTitle}`}
+                        className="inline-block w-11 h-11 bg-[rgba(104,110,226,0.1)] text-primary text-base rounded-[3px] leading-[47px] text-center xs:absolute top-0 bottom-0 right-5  xs:m-auto mt-3 hover:bg-primary hover:text-white"
+                      >
+                        <FaArrowRight className="inline-block m-auto" />
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </figure>;
+              </figure>
+            );
           })}
         </div>
       </div>
